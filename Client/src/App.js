db03018558bb92f1a43214127fbde2512a780d55
ranaofficial1 <ranaofficial54@gmail.com>
2022-04-16 17:45:43 +0530

    initial

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './component/Header';
 import Navbar from './component/Navbar';
-import Homepage from './component/Homepage';
-import RegistrationForm from './component/RegistrationForm';
-import Products from './component/Products';
-import Footer from './component/Footer';
-import LoginForm from './component/LoginForm';
-import AdminLogin from './component/AdminLogin';
 import LoginRestriction from './component/LoginRestriction';
 import PrivateRoute from './component/PrivateRoute';
-import Cart from './component/Cart';
-import AdminWork from './component/AdminWork';
-import Profile from './component/Profile';
 import { useSelector } from 'react-redux';
-import Loginregis from './component/Login';
 import CheckoutPage from './component/CheckoutPage';
 import Catgories from './component/Categories';
 
@@ -27,31 +16,16 @@ function App() {
 
   return (
     <div className='App'>
-      {/* <Header/> */}
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Homepage />} />
-        <Route exact path='/products' element={<Products />} />
-        <Route exact path='/adminwork' element={<AdminWork />} />
-        <Route exact path='/admin' element={<AdminLogin />} />
         <Route exact path='/checkout' element={<CheckoutPage />} />
         {/* <Route exact path ="/navbar" element={<Navbar/>}/> */}
         <Route exact path='/categories' element={<Catgories />} />
 
-        <Route element={<LoginRestriction />}>
-          <Route exact path='/register' element={<RegistrationForm />} />
-          {/* <Route exact path="/login" element={<LoginForm/>}/> */}
-          <Route exact path='/login' element={<Loginregis />} />
-        </Route>
+        <Route element={<LoginRestriction />}></Route>
 
-        <Route element={<PrivateRoute />}>
-          <Route exact path='/profile' element={<Profile />} />
-          <Route exact path='/cart' element={<Cart />} />
-        </Route>
+        <Route element={<PrivateRoute />}></Route>
       </Routes>
-      <div className=''>
-        <Footer />
-      </div>
     </div>
   );
 }
