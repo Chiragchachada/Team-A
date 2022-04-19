88562c8fbd07bf863c0ec9e382b66100660bac0d
Shubham <shubhamgour08@gmail.com>
2022-04-19 11:54:48 +0000

    UI added

--- Client/src/component/shop/Product.js
+++ Client/src/component/shop/Product.js
@@ -3,7 +3,7 @@ import {Modal} from "react-bootstrap";
 // import '../../css/Style.css'
 
 
-export default function Product({ product }) {
+export default function Product({ product, search }) {
   const [quantity, setquantity] = useState();
   const [varient, setvarient] = useState("small");
 
@@ -12,13 +12,23 @@ export default function Product({ product }) {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+
+
+
+
+
+
   return (
+    <>
+    
+{/* Page Product design start */}
     <div
-      style={{ margin: "50px", borderRadius:'40px' }}
-      className="shadow-lg p-3 mb-5 bg-white  cat-item "
+      style={{  borderRadius:'40px', margin: '0px' }}
+      className="shadow-lg   mb-5  bg-white  cat-item "
     >
-     <div onClick={handleShow}  style={{cursor:'pointer'}} className='cat-item'>
-     <h1 id="small">{product.title}</h1>
+     <div onClick={handleShow}  style={{cursor:'pointer'}} className=' '>
+    
+     <h1 id="small" >{product.title}</h1>
       <img
         src={product.image}
         className="img-fluid cat-img mx-auto "
@@ -29,7 +39,7 @@ export default function Product({ product }) {
         <div className="w-100 m-1">
           {/* <p>SIZE</p> */}
          <div className="w-100 d-flex m-1">
-          <p className="mt-3  text-muted">Quantity</p>
+          <p className="mt-3  text-muted" style={{ fontSize: '14px'}}>Quantity</p>
           <select 
             className="form-control mx-auto p-0 text-center mt-2 shadow rounded  text-muted" 
             value={quantity} style={{cursor:'pointer',width:"40px"}}
@@ -44,7 +54,7 @@ export default function Product({ product }) {
         </div>
         </div>
         <div className=" mt-3 w-100">
-          <h1 id="small" className="mt-2 text-muted" style={{cursor:'pointer'}}>Price: {product.price}₹</h1>
+          <h1  className="mt-2 text-muted" style={{cursor:'pointer', fontSize: '14px'}}>Price: {product.price}₹</h1>
           
         </div>
         
@@ -73,6 +83,8 @@ export default function Product({ product }) {
   
 </Modal>
     </div>
+    {/* Page Product design end */}
+    </>
   );
 }
 
