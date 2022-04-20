c5cb87cb2f53327c02418d1140fcaeb0b685fb3d
Chirag Chachada <chachada.chirag@gmail.com>
2022-04-20 17:02:10 +0530

    review added

--- Client/src/component/shop/Homescreen.js
+++ Client/src/component/shop/Homescreen.js
@@ -16,12 +16,12 @@ export default function Homescreen() {
   
   });
 
-  // for (let i = 0; i < shop.length; i++) {
-  //   let dt = shop[i].products;
-  //   for (let j = 0; j < dt.length; j++) {
-  //     ShopData.push(dt[j]);
-  //   }
-  // }
+  for (let i = 0; i < shop.length; i++) {
+    let dt = shop[i].products;
+    for (let j = 0; j < dt.length; j++) {
+      ShopData.push(dt[j]);
+    }
+  }
 
 const [...final] = shop
 
